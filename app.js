9a98b73f3edbb845516885f507fb3450433e4597
Livia_Santos <liviamelq@gmail.com>
2018-11-23 12:49:35 +1100

    Add ui and on click event

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
+// gets a new object
 var g = G$ ('Jonh', 'Doe')
-g.greet();
+
+// use chainable methods
+g.greet().setLang('pt').greet('formal').log();
+
+// let's use our objcet on the click of the login button
+$('#login').click(function() {
+    var loginGrtr = G$('Jonh', 'Doe');
+
+    $('#logindiv').hide();
+
+    loginGrtr.setLang($('#lang').val()).HTMLGreeting('#greeting', true).log();
+
+});
